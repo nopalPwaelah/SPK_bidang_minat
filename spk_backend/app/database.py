fbe77120d4683cc39cd626a29d93e776acf053af
nopalPwaelah <naufal17akbar08@gmail.com>
2026-05-23 02:15:47 +0700

    MANUT

--- spk_backend/app/database.py
+++ spk_backend/app/database.py
@@ -9,7 +9,18 @@ print("DATABASE_URL =", DATABASE_URL)
 if not DATABASE_URL:
     raise Exception("DATABASE_URL tidak ditemukan di Environment Variables")
 
-engine = create_engine(DATABASE_URL)
+# Railway → SQLAlchemy → pakai PyMySQL
+if DATABASE_URL.startswith("mysql://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "mysql://",
+        "mysql+pymysql://",
+        1
+    )
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
