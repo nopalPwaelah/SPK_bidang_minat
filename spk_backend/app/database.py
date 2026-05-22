c8be708a31ce6ddf81a18d4ccfe10f68ba3ee94c
nopalPwaelah <naufal17akbar08@gmail.com>
2026-05-22 22:14:58 +0700

    use railway database url

--- spk_backend/app/database.py
+++ spk_backend/app/database.py
@@ -6,12 +6,15 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 print("DATABASE_URL =", DATABASE_URL)
 
+if not DATABASE_URL:
+    raise Exception("DATABASE_URL tidak ditemukan di Environment Variables")
+
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
-    bind=engine
+    bind=engine,
 )
 
 Base = declarative_base()
