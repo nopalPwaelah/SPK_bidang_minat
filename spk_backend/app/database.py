fdebfd3cddf9c4e7269158ac0dff4c5ad74bc0a5
nopalPwaelah <naufal17akbar08@gmail.com>
2026-05-22 21:27:51 +0700

    fix railway deployment and database

--- spk_backend/app/database.py
+++ spk_backend/app/database.py
@@ -4,6 +4,8 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+print("DATABASE_URL =", DATABASE_URL)
+
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
