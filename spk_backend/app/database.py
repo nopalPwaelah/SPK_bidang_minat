8ae73f0f485e5e555f738e04155068fbef33631d
nopalPwaelah <naufal17akbar08@gmail.com>
2026-05-22 13:47:51 +0700

    dahlah

--- spk_backend/app/database.py
+++ spk_backend/app/database.py
@@ -2,7 +2,13 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://root:@localhost:3306/spk_knn")
+MYSQL_HOST = os.getenv("MYSQLHOST", "localhost")
+MYSQL_PORT = os.getenv("MYSQLPORT", "3306")
+MYSQL_USER = os.getenv("MYSQLUSER", "root")
+MYSQL_PASSWORD = os.getenv("MYSQLPASSWORD", "")
+MYSQL_DATABASE = os.getenv("MYSQL_DATABASE", "spk_knn")
+
+DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(
