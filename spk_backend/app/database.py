f737edcfa8697d5c872969463faa164516c70b8d
nopalPwaelah <naufal17akbar08@gmail.com>
2026-05-22 13:41:02 +0700

    dan

--- spk_backend/app/database.py
+++ spk_backend/app/database.py
@@ -1,7 +1,8 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:@localhost:3306/spk_knn"
+DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://root:@localhost:3306/spk_knn")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(
