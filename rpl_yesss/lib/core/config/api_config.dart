f9ff52657d2784c3bef1efd5123a6d0ce158bafe
nopalPwaelah <naufal17akbar08@gmail.com>
2026-05-23 20:10:06 +0700

    bismillah

--- rpl_yesss/lib/core/config/api_config.dart
+++ rpl_yesss/lib/core/config/api_config.dart
@@ -2,7 +2,7 @@ class ApiConfig {
   // 🔥 GANTI SESUAI DEVICE
 
   // ✅ HP / WIFI (PUNYA KAMU)
-  static const String baseUrl = "http://127.0.0.1:8000";
+  static const String baseUrl = "https://spkbidangminat-production.up.railway.app";
 
   // ✅ Emulator (kalau pakai)
   // static const String baseUrl = "http://10.0.2.2:8000";
